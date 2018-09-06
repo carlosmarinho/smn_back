76653abe3074954cebbaf17a78ce6b4a653a5b2d
cmarinho-at-688330617832 <carlos.marinho.contratada@lasa.com.br>
2018-09-06 18:06:27 -0300

    email, enum e outros

--- models/User.js
+++ models/User.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
+const {isEmail} = require('validator');
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema ({
-    image: {type: String, image:true},
-    status: {type: Boolean},
+    image: {type: String, image:true, featured: true},
     username: {type: String, required: true, unique: "Username '{VALUE}' já está em uso. Favor escolher outro!"},
+    email: {type: String, required: true, unique: "Email '{email}' já está cadastado!", validate: [ isEmail, 'Email inválido' ]},
     first_name: {type: String, required: "Primeiro nome é obrigatório"},
     last_name: {type: String, required: false},
-    age: {type: Number, min: [18, "Você deve ser maior de 18 anos"], max: [80, "você não deve ser maior que 80 anos"] }
+    age: {type: Number, min: [18, "Você deve ser maior de 18 anos"], max: [80, "você não deve ser maior que 80 anos"] },
+    type: {type: String, enum: ['administrador', 'editor', 'usuário'], default: 'usuário'},
+    status: {type: Boolean, default: true},
+
+    //Se quiser o valor não ser obrigatório coloque o campo ''
+    //type: {type: String, enum: ['', 'administrador', 'editor', 'usuário'], default: 'usuário'},
+
+    //Se eu quiser formar o tipo que o elemento vai ser no formulário é só usar o campo inputForm
+    //type: {type: String, enum: ['administrador', 'editor', 'usuário'], default: 'user', inputForm: 'radio'},
+    //status: {type: Boolean, inputForm: 'select'},
 }) 
 
 userSchema.plugin(beautifyUnique);
