46ad0a09154ff417f5e1149e6ae474a9e2e19dd7
cmarinho-at-688330617832 <carlos.marinho.contratada@lasa.com.br>
2018-09-12 18:50:35 -0300

    Iniciando a migração do usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,8 @@
 const controller = require('../controllers/userController')
 
 module.exports = (app, upload) => {
-    app.delete("/users", (req, res) => controller.deleteUsers(req, res) )
-    app.delete("/users/:id", (req, res) => controller.deleteUser(req, res) )
+  
     app.get("/users", (req, res) => controller.viewAll(req, res) )
-    app.get("/users/fields", (req, res) => controller.getAllFields(req, res) )
-    app.get("/users/:id", (req, res) => controller.view(req, res) )
-    app.put("/users/update-field", (req, res) => controller.updateField(req, res) )
-    app.put("/users/:id", upload.array('files'), (req, res) => controller.edit(req, res) )
-    //app.post("/users", upload.single('files'), (req, res) => controller.add(req, res) )
-    app.post("/users", upload.array('files'), (req, res) => controller.add(req, res) )
-    app.post("/login", (req, res) => controller.login(req, res) )
+    app.get("/users/migrate", (req, res) => controller.migrate(req, res) )
+
 }
